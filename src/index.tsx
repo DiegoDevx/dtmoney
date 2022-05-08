f7eb4889392cb2cfc7751bde31b121b588f59e15
Diego Mendes <diego.developerx@gmail.com>
2022-05-08 12:28:21 +0200

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as ReactDOM from 'react-dom/client';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+        title: 'Freelancer de website',
+        type: 'deposit',
+        category: 'Desenvolvimento',
+        amount: 6000,
+        createdAt: new Date('2022-05-03 09:00:47'),
+        },
+        {
+          id: 2,
+        title: 'Aulas de Inglês',
+        type: 'withdraw',
+        category: 'Estudos',
+        amount: 700,
+        createdAt: new Date('2022-04-13 10:37:47'),
+        },
+      ],
+    })
+  },
+  
+  routes() {
+
+
+    this.namespace = 'api';
+
+    this.get('/transactions', () =>{
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
+  }
+});
+
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
